6e8d16fe1a89fc48abf9492edd3b06ed0ec1ff2c
Kyle Robinson <robinsonkyle7398@gmail.com>
2018-09-27 19:56:43 -0400

    commented out route files so does not throw error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,82 +1,82 @@
-// var db = require("../models");
+// // var db = require("../models");
 
-module.exports = function(app) {
-  // Get all posts
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+// module.exports = function(app) {
+//   // Get all posts
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Post.create({
-      //title: req.body.title,
-      //body: req.body.body,
-      //category: req.body.category
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
+//   // POST route for saving a new post
+//   app.post("/api/posts", function(req, res) {
+//     console.log(req.body);
+//     db.Post.create({
+//       //title: req.body.title,
+//       //body: req.body.body,
+//       //category: req.body.category
+//     })
+//       .then(function(dbPost) {
+//         res.json(dbPost);
+//       });
+//   });
 
-  // Delete a post by id (must be user that creates ad)
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy(
-      { 
-      where: { 
-        id: req.params.id 
-      } 
-    }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+//   // Delete a post by id (must be user that creates ad)
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy(
+//       { 
+//       where: { 
+//         id: req.params.id 
+//       } 
+//     }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
 
-// update ad that user created 
-app.put("/api/examples/:id", function (req, res) {
-  db.Example.update(req.body, 
-    {
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(function(dbExample) {
-    res.json(dbExample);
-  })
-})
+// // update ad that user created 
+// app.put("/api/examples/:id", function (req, res) {
+//   db.Example.update(req.body, 
+//     {
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//   .then(function(dbExample) {
+//     res.json(dbExample);
+//   })
+// })
 
-// filter based on category 
-app.get("/api/examples/:category", function(req, res) {
-  db.Example.findAll({
-    where: {
-      category: req.params.category
-    }
-  }).then(function(dbExamples) {
-    res.json(dbExamples);
-  });
-});
+// // filter based on category 
+// app.get("/api/examples/:category", function(req, res) {
+//   db.Example.findAll({
+//     where: {
+//       category: req.params.category
+//     }
+//   }).then(function(dbExamples) {
+//     res.json(dbExamples);
+//   });
+// });
 
-// filter based on price 
-app.get("/api/examples/:price", function(req, res) {
-  db.Example.findAll({
-    where: {
-      price: req.params.price
-    }
-  }).then(function(dbExamples) {
-    res.json(dbExamples);
-  });
-});
+// // filter based on price 
+// app.get("/api/examples/:price", function(req, res) {
+//   db.Example.findAll({
+//     where: {
+//       price: req.params.price
+//     }
+//   }).then(function(dbExamples) {
+//     res.json(dbExamples);
+//   });
+// });
 
-// filter based on user rating 
-app.get("/api/examples/:rating", function(req, res) {
-  db.Example.findAll({
-    where: {
-      rating: req.params.rating
-    }
-  }).then(function(dbExamples) {
-    res.json(dbExamples);
-  });
-});
+// // filter based on user rating 
+// app.get("/api/examples/:rating", function(req, res) {
+//   db.Example.findAll({
+//     where: {
+//       rating: req.params.rating
+//     }
+//   }).then(function(dbExamples) {
+//     res.json(dbExamples);
+//   });
+// });
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,72 +1,72 @@
-var db = require("../models");
+// var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         examples: dbExamples
+//       });
+//     });
+//   });
 
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/index/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/index/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/individual-ad/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/individual-ad/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/login/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/login/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/Post-ad/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/Post-ad/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
 
-  // Load profile page and pass in an example by id
-  app.get("/profile/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-    console.log(products_data);
-  });
+//   // Load profile page and pass in an example by id
+//   app.get("/profile/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//     console.log(products_data);
+//   });
 
-  // display user information (profile)
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-};
+//   // display user information (profile)
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
+// };
 
-module.exports = router;
+// module.exports = router;
