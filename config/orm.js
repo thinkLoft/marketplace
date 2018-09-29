e27e862b6c6c6ebc3094cf803a3474496dc00458
Parth Patel <pvpprod89@gmail.com>
2018-09-28 23:45:07 -0400

    made new database with userID from user table linked to product table, and made ORM queries

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,91 @@
-var connection = require('./connection.js');
+var connection = require("./connection.js");
 
 var orm = {
-  all: function (tableInput, cb) {
-    connection.query('SELECT * FROM ' + tableInput + ';', function (err, result) {
-      if (err) throw err;
-      cb(result)
-    })
+  selectAll: function(tableInput, cb) {
+    connection.query("select * from " + tableInput + ";", function(
+      err,
+      result
+    ) {
+      // if (err) throw err;
+      console.log("[mysql error]", err);
+      cb(result);
+    });
+  },
+
+  insertProduct: function(tableInput, imageURL, title, description, category, price, userID, cb) {
+    connection.query(
+      "insert " +
+        "into " +
+        tableInput +
+        " " +
+        "(image, title, description, category, price, userID, sold) values ('"+imageURL+"', '"+title+"', '"+description+"', '"+category+"', " + price + ", " + userID + ");",
+      function(err, result) {
+        // if (err) throw err;
+        console.log("[mysql error]", err);
+        cb(result);
+      }
+    );
+  },
+
+  insertUser: function(tableInput, firstName, lastName, email, password, cb) {
+    connection.query(
+      "insert " +
+        "into " +
+        tableInput +
+        " " +
+        "(firstName, lastName, email, password) values ('"+firstName+"', '"+lastName+"', '"+email+"', '"+password+"');",
+      function(err, result) {
+        // if (err) throw err;
+        console.log("[mysql error]", err);
+        cb(result);
+      }
+    );
+  },
+
+  updateSold: function(tableInput, condition, cb) {
+    connection.query(
+      "update " +
+        tableInput +
+        " " +
+        "set sold=true where id=" +
+        condition +
+        " ;",
+      function(err, result) {
+        // if (err) throw err;
+        console.log("[mysql error]", err);
+        cb(result);
+      }
+    );
+  },
+
+  updateProduct: function(tableInput, imageURL, title, description, category, price, id, cb) {
+    connection.query(
+      "update " +
+        tableInput +
+        " " +
+        "set image = "+ '"'+imageURL+'"' + ", title = "+ '"'+title+'"' + ", description = "+ '"'+description+'"' + ", category = "+ '"'+category+'"' + ", price = "+price+ " where id= "+id+ ";",
+      function(err, result) {
+        // if (err) throw err;
+        console.log("[mysql error]", err);
+        cb(result);
+      }
+    );
+  },
+
+  updateUser: function(tableInput, firstName, lastName, email, password, userID, cb) {
+    connection.query(
+      "update " +
+        tableInput +
+        " " +
+        "set firstName = "+ '"'+firstName+'"' + ", lastName = "+ '"'+lastName+'"' + ", email = "+ '"'+email+'"' + ", password = "+ '"'+password+'"' + " where userID= "+userID+ ";",
+      function(err, result) {
+        // if (err) throw err;
+        console.log("[mysql error]", err);
+        cb(result);
+      }
+    );
   }
 
-  //,
-
-  // update: function (tableInput, condition, cb) {
-  //   connection.query('UPDATE ' + tableInput + ' SET devoured=true WHERE id= ' + condition + ';', function (err, result) {
-  //     if (err) throw err;
-  //     cb(result);
-  //   })
-  // },
-
-  // create: function (tableInput, val, cb) {
-  //   connection.query('INSERT INTO ' + tableInput + " (burger_name) VALUES ('" + val + "');", function (err, result) {
-  //     if (err) throw err;
-  //     cb(result);
-  //   })
-  // }
-}
-
+};
 
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -3,45 +3,51 @@ CREATE DATABASE marketplacedb;
 
 USE marketplacedb;
 
-CREATE TABLE products
+CREATE TABLE user
 (
-  id MEDIUMINT
+  userID MEDIUMINT
   AUTO_INCREMENT NOT NULL,
-image VARCHAR
-  (2000) NOT NULL,
-title VARCHAR
-  (100) NOT NULL, 
-description VARCHAR
+firstName VARCHAR
   (255) NOT NULL,
-category VARCHAR
+lastName VARCHAR
+  (255) NOT NULL, 
+email VARCHAR
+  (255) NOT NULL,
+password VARCHAR
   (100) NOT NULL,
-price DECIMAL
-  (10,2) NOT NULL,
-userID VARCHAR
-  (100) NOT NULL,
-sold boolean DEFAULT FALSE,
 primary key
-  (id)
+  (userID)
 );
 
   select *
-  from products;
+  from user;
 
-  CREATE TABLE user
+  CREATE TABLE products
   (
-    userID MEDIUMINT
+    id MEDIUMINT
     AUTO_INCREMENT NOT NULL,
-firstName VARCHAR
+image VARCHAR
+    (2000) NOT NULL,
+title VARCHAR
+    (100) NOT NULL, 
+description VARCHAR
     (255) NOT NULL,
-lastName VARCHAR
-    (255) NOT NULL, 
-email VARCHAR
-    (255) NOT NULL,
-password VARCHAR
+category VARCHAR
     (100) NOT NULL,
+price DECIMAL
+    (10,2) NOT NULL,
+userID MEDIUMINT NOT NULL,
+sold boolean DEFAULT FALSE,
 primary key
+    (id),
+constraint fk_userID foreign key
     (userID)
-);
+references user
+    (userID)
+on
+    delete cascade on
+    update cascade);
+
 
     select *
-    from user;
+    from products;
