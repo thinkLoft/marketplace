616a1c27dea409870f3c7de549254267ecbe692e
Parth Patel <pvpprod89@gmail.com>
2018-10-04 18:26:04 -0400

    added HTML Route to Server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   bodyParser.urlencoded({
