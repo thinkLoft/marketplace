4b7d1d77208d19da8bcb1b2c87d76823337e3a5e
thinkLoft <kareem.lofters@gmail.com>
2018-10-04 21:44:47 -0400

    completed pulling data to the front

--- public/javascript/loadIndex.js
+++ public/javascript/loadIndex.js
@@ -14,22 +14,10 @@ $(document).ready(function() {
                postprice = ad.price;
                user = ad.userID;
                sold = ad.sold;
-               postimageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqZciSJH_AN2H-czNXGJFIqgyxy7uN0KhfMTNbTg7mQW277DhCpw"
+               postimageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqZciSJH_AN2H-czNXGJFIqgyxy7uN0KhfMTNbTg7mQW277DhCpw";
+             
+               html = '<div class="row"><div class="col-sm-4 indexImage"><img src="' + postimageURL + '" alt="cottage" width="200" height="200"></div><div class="col-sm-8"><div class="row"><div class="col title">' + posttitle + '</div></div><div class="row"><div class="col description">' + postdescription.slice(0, 250) + '</div></div><br><div class="row"><div class="col category">' + category + '</div></div><div class="row"><div class="col readMore"><a href="/api/posts/' + postID + '">Read More</a></div></div><br><div class="row"><div class="col price">' + postprice + '</div></div></div></div>'
 
-               html = 
-               '<div class="content"><div class="ui items"><div class="item"><div class="postItemImage ui small image"><img src="' +
-               postimageURL +
-               '"></div><div class="content" style="padding: 1rem;"><h2 class="header postTitle">' +
-               posttitle +
-               '</h2><div class="postDescription"><p>' +
-               postdescription.slice(0, 250) +
-               " . . .   <br> <br><a href='individualAd.html?postid=" +
-               postID +
-               "'> Read More</a>" +
-               '</p></div><br><div class="postPrice"><h3>Price: $<span class="price">' +
-               postprice +
-               '</span></h3></div></div></div></div></div>';
-               
                $('#homepage').append(html);
             });
         });
