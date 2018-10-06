7e8cdcbc81cdba4613b98507995c20b3e0cc1395
Parth Patel <pvpprod89@gmail.com>
2018-10-06 14:29:28 -0400

    changed createAccount file and apiRoutes to send data to database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,10 @@
-var db = require("../models/models");
+// var db = require("../models/models");
+var connection = require("../config/connection.js");
 var express = require("express");
 var app = express.Router();
+var bodyParser = require("body-parser");
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Get all POSTS
 app.get("/", function(req, res) {
@@ -11,27 +15,38 @@ app.get("/", function(req, res) {
 });
 
 // POST route for a new user
-app.post("/api/posts/createUser", function(req, res) {
+app.post("/api/posts/createUser", urlencodedParser, function(req, res) {
   console.log(req.body);
-  db.insertUserUser({
-    firstName: req.params.firstName,
-    lastName: req.params.lastName,
-    email: req.params.email,
-    password: req.params.password,
-    userID: req.params.userID
-  });
+  // res.render("createAccount.html", { qs: req.query });
+
+  if (!req.body) return res.sendStatus(400);
+  // res.send("welcome, " + req.body.firstName);
+
+  var queryString =
+    "INSERT INTO user (firstName, lastName, email) VALUES (?,?,?)";
+
+  connection.query(
+    queryString,
+    [req.body.firstName, req.body.lastName, req.body.email],
+    function(err, result) {
+      if (err) throw err;
+      console.log("User Successfully Added!", result);
+      res.end();
+    }
+  );
 });
 
 // POST route for saving a new post
 app.post("/api/posts/createPost", function(req, res) {
   console.log(req.body);
-  db.insertProductsProducts({
-    image: req.body.image,
-    title: req.body.title,
-    description: req.body.description,
-    category: req.body.category,
-    price: req.body.price
-  });
+
+  // db.insertProductsProducts({
+  //   image: req.body.image,
+  //   title: req.body.title,
+  //   description: req.body.description,
+  //   category: req.body.category,
+  //   price: req.body.price
+  // });
 });
 
 // Delete a post by id (must be user that creates ad)
