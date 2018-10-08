e7ab1a261a8a97932eab32ad4f10e7b22e683e23
Parth Patel <pvpprod89@gmail.com>
2018-10-07 21:16:07 -0400

    changed some queries and table columns in schema and seeds, ad.image in loadIndex, Email input in postAd, matching style in css, and added some SQL queries in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,13 +10,24 @@ module.exports = function(app) {
   /////////// POST INFORMATION \\\\\\\\\
 
   // Get all posts
-  app.get("/api/posts/", function(req, res) {
-    db.selectAllProducts(function(data) {
-      res.json(data);
+  app.get("/api/posts/", urlencodedParser, function(req, res) {
+    console.log(req.body);
+    // res.render("createAccount.html", { qs: req.query });
+
+    if (!req.body) return res.sendStatus(400);
+    // res.send("welcome, " + req.body.firstName);
+
+    var queryString = "SELECT * FROM products";
+
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      console.log("Products Available!", result);
+      res.json(result);
+      res.end();
     });
   });
 
-  // POST route for a new user
+  // POST route for creating a new user
   app.post("/api/posts/createUser", urlencodedParser, function(req, res) {
     console.log(req.body);
     // res.render("createAccount.html", { qs: req.query });
@@ -33,91 +44,143 @@ module.exports = function(app) {
       function(err, result) {
         if (err) throw err;
         console.log("User Successfully Added!", result);
+        // res.json(result);
+        res.redirect("/");
         res.end();
       }
     );
   });
 
-  // POST route for saving a new post
-  app.post("/api/posts/createPost", function(req, res) {
+  // POST route for creating a new post
+  app.post("/api/posts/createPost/", urlencodedParser, function(req, res) {
     console.log(req.body);
+
+    if (!req.body) return res.sendStatus(400);
+
+    var queryString =
+      "INSERT INTO products (image, title, description, category, price, email) VALUES (?,?,?,?,?,?)";
+
+    connection.query(
+      queryString,
+      [
+        req.body.image,
+        req.body.title,
+        req.body.description,
+        req.body.category,
+        req.body.price,
+        req.body.email
+      ],
+      function(err, result) {
+        if (err) throw err;
+        console.log("Ad/Post Successfully Added!", result);
+        res.redirect("/");
+        res.end();
+      }
+    );
+  });
+
+  // POST route for updating a post by id
+  app.post("/api/posts/createPost/:id", urlencodedParser, function(req, res) {
+    console.log(req.body);
+
+    if (!req.body) return res.sendStatus(400);
+
+    var queryString =
+      "UPDATE products SET (image, title, description, category, price, email) VALUES (?,?,?,?,?,?,) WHERE id=?";
+
+    connection.query(
+      queryString,
+      [
+        req.body.image,
+        req.body.title,
+        req.body.description,
+        req.body.category,
+        req.body.price,
+        req.body.email,
+        req.params.id
+      ],
+      function(err, result) {
+        if (err) throw err;
+        console.log("Ad/Post Successfully Updated!", result);
+        res.redirect("/");
+        res.end();
+      }
+    );
   });
 
   // update ad created by unique user
-  app.put("/api/post/:id", function(req, res) {
-    db.updateProductsProducts(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
-  });
+  // app.put("/api/post/:id", function(req, res) {
+  //   db.updateProductsProducts(req.body, {
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
+  // });
 
   // Delete a post by id (must be user that creates ad)
-  app.delete("/api/posts/:id", function(req, res) {
-    db.deleteProductProduct({
-      where: {
-        id: req.params.id
-      }
-    });
-  });
+  // app.delete("/api/posts/:id", function(req, res) {
+  //   db.deleteProductProduct({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
+  // });
 
   /////// FILTER and SEARCH BAR \\\\\\\\\\
 
   // filter based on category
-  app.get("/api/posts/:category", function(req, res) {
-    db.selectAllProducts({
-      where: {
-        category: req.params.category
-      }
-    });
-  });
+  // app.get("/api/posts/:category", function(req, res) {
+  //   db.selectAllProducts({
+  //     where: {
+  //       category: req.params.category
+  //     }
+  //   });
+  // });
 
   // filter based on price
-  app.get("/api/post/:price", function(req, res) {
-    db.selectAllProducts({
-      where: {
-        price: req.params.price
-      }
-    });
-  });
+  // app.get("/api/post/:price", function(req, res) {
+  //   db.selectAllProducts({
+  //     where: {
+  //       price: req.params.price
+  //     }
+  //   });
+  // });
 
   // search bar (search by title)
-  app.get("/api/post/:title", function(req, res) {
-    db.selectAllProducts({
-      where: {
-        title: req.params.title
-      }
-    });
-  });
+  // app.get("/api/post/:title", function(req, res) {
+  //   db.selectAllProducts({
+  //     where: {
+  //       title: req.params.title
+  //     }
+  //   });
+  // });
 
   ////////// USER INFORMATION \\\\\\\\\\
 
   // find and display user information
-  app.get("/api/user/:id", function(req, res) {
-    db.selectAllUser({
-      where: {
-        userid: req.params.id
-      }
-    });
-  });
+  // app.get("/api/user/:id", function(req, res) {
+  //   db.selectAllUser({
+  //     where: {
+  //       userid: req.params.id
+  //     }
+  //   });
+  // });
 
   // update user information
-  app.put("/api/user/:id", function(req, res) {
-    db.updateUserUsert({
-      where: {
-        userid: req.params.id
-      }
-    });
-  });
+  // app.put("/api/user/:id", function(req, res) {
+  //   db.updateUserUsert({
+  //     where: {
+  //       userid: req.params.id
+  //     }
+  //   });
+  // });
 
   // Delete account by user id
-  app.delete("/api/user/:id", function(req, res) {
-    db.deleteUserUser({
-      where: {
-        id: req.params.id
-      }
-    });
-  });
+  // app.delete("/api/user/:id", function(req, res) {
+  //   db.deleteUserUser({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
+  // });
 };
-
-module.exports = app;
